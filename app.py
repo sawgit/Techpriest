31715d85034f118e088b3ce9281b607a30d0e4b8
Stu <saw.signups@outlook.com>
2020-05-19 17:47:07 +0100

    BuildRaceList fleshed out with better support for team allocation

--- app.py
+++ app.py
@@ -19,15 +19,46 @@ def ChooseRace(races):
     chosenRace = random.choice(races)
     return chosenRace
 
-def BuildRaceList(players = 2):
+def BuildRaceList(players = 2, teams = 2):
+#Function will distribute a number of players to a set number of teams.
 
-    count = 0
-    racelist = []
-    while count < players:
-        racelist.append(ChooseRace(races))
-        count += 1
+    def calcTeamsize(players, teams):
+    #Function will recieve a number of players and divide that by the number of teams.
+    #Will produce an error if number is not divisable.
+        modulo = teams / players
 
-    return racelist
+        if isinstance(modulo, float) == True:
+            modulo = 0
+
+        return modulo
+
+    playerCount = 0
+    teamSize = calcTeamsize(players, teams)
+    teamList0 = []
+    teamList1 = []
+    teamList2 = []
+
+    while playerCount < players:
+        print("Debug: " + str(playerCount) + str(players) + str(teamSize))
+
+        while playerCount < teamSize:
+            print("DebugX02: " + str(playerCount) + str(players))
+            teamList0.append(ChooseRace(races))
+            playerCount += 1
+
+        while playerCount < teamSize:
+            print("DebugX03: " + str(playerCount) + str(players))
+            teamList1.append(ChooseRace(races))
+            playerCount += 1
+
+        while playerCount < teamSize:
+            teamList2.append(ChooseRace(races))
+            playerCount += 1
+
+    #This section will receive the individual racelists, collate then format them for output.
+    formattedOutput = 'Team 1: ' + str(teamList0) + 'Team 2: ' + str(teamList1) + 'Team 3: ' + str(teamList2)
+
+    return formattedOutput
 
 def SelectFromTxtFile():
 #Function will return a random word from word_list.txt
@@ -50,7 +81,7 @@ async def on_message(message):
         return
 
     elif message.content.startswith('tp match'):
-        await message.channel.send('I have completed the task... \n  '+ str(BuildRaceList()))
+        await message.channel.send('I have completed the task... \n  '+ BuildRaceList())
 
     elif message.content.startswith('tp quote'):
         await message.channel.send(SelectFromTxtFile())
